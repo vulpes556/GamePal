1ce26374248a6909b435aeb3026931d4bbabfe01
Bálint <szelasszie@gmail.com>
2025-05-19 12:11:34 +0200

    added some content to the main page

--- game-pal-frontend/app/page.jsx
+++ game-pal-frontend/app/page.jsx
@@ -1,7 +1,12 @@
 
 export default function Home() {
   return (
-    <>Hello World!
-    </>
+    <div className="main-page">
+      <div className="welcome-div">
+        <h1>Find your gaming buddies</h1>
+        <p>Connect with other players and enjoy your favourite games together</p>
+        <input type="text" placeholder="Search for games" />
+      </div>
+    </div>
   );
 }
