1104c72816abf677984fecbadb661b8908f28120
Bálint <szelasszie@gmail.com>
2025-05-21 12:42:32 +0200

    add logic behind get all games endpoint (repo, service)

---
+++ GamePalBackend/GamePal/Repositories/GameRepo/GameRepo.cs
@@ -0,0 +1,20 @@
+﻿using GamePal.Context;
+using GamePal.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamePal.Repositories.GameRepo
+{
+    public class GameRepo : IGameRepo
+    {
+        private readonly DBContext _dbContext;
+
+        public GameRepo(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Game>> GetAllAsync()
+        {
+            return await _dbContext.Games.ToListAsync();
+        }
+    }
+}
--- GamePalBackend/GamePal/Services/GameServices/GameService.cs
+++ GamePalBackend/GamePal/Services/GameServices/GameService.cs
@@ -1,12 +1,35 @@
-﻿using GamePal.Models.GameModels;
+﻿using GamePal.Data.Entities;
+using GamePal.Models.GameModels;
+using GamePal.Repositories.GameRepo;
 
 namespace GamePal.Services.GameServices
 {
     public class GameService : IGameService
     {
-        public Task<IEnumerable<GameDTO>> GetAllAsync()
+
+        IGameRepo _gameRepository;
+
+        public GameService(IGameRepo gameRepo)
         {
-            throw new NotImplementedException();
+            _gameRepository = gameRepo;
+        }
+        public async Task<IEnumerable<GameDTO>> GetAllAsync()
+        {
+            var games = await _gameRepository.GetAllAsync();
+            return games.Select(ModelToDTOConverter);
+        }
+
+
+
+        private GameDTO ModelToDTOConverter(Game game)
+        {
+            return new GameDTO()
+            {
+                Id = game.Id,
+                Name = game.Name,
+                Platform = game.Platform,
+            };
         }
     }
+
 }
