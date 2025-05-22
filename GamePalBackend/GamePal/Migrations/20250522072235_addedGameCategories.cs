5bdc250623c009eed80e33b3b033e7b1d3dea531
Bálint <szelasszie@gmail.com>
2025-05-22 09:25:27 +0200

    migrated changes to db

---
+++ GamePalBackend/GamePal/Migrations/20250522072235_addedGameCategories.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GamePal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedGameCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameGameCategory",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "integer", nullable: false),
+                    GamesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameGameCategory", x => new { x.CategoriesId, x.GamesId });
+                    table.ForeignKey(
+                        name: "FK_GameGameCategory_GameCategories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "GameCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameGameCategory_Games_GamesId",
+                        column: x => x.GamesId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGameCategory_GamesId",
+                table: "GameGameCategory",
+                column: "GamesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameGameCategory");
+
+            migrationBuilder.DropTable(
+                name: "GameCategories");
+        }
+    }
+}
