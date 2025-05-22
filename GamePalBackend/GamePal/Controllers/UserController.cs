6f7b4f874d6e2f49fabab1f92a5305b313e0fa59
Bálint <szelasszie@gmail.com>
2025-05-22 11:08:45 +0200

    added register endpoint

---
+++ GamePalBackend/GamePal/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using GamePal.Models.AuthContracts;
+using GamePal.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamePal.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        IUserService _userService;
+        ILogger<UserController> _logger;
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest registrationRequest)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                const string role = "User";
+                var result = await _userService.RegisterAsync(registrationRequest, role);
+
+                if (!result.Success)
+                {
+                    AddErrors(result);
+                    return BadRequest(ModelState);
+                }
+
+                return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+        private void AddErrors(AuthResult result)
+        {
+            foreach (var error in result.ErrorMessages)
+                ModelState.AddModelError(error.Key, error.Value);
+        }
+    }
+}
