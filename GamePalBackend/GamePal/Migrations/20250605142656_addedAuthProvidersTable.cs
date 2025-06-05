708792bdfb1142216d1bc66458f9adc9e3486a24
Bálint <szelasszie@gmail.com>
2025-06-05 16:40:41 +0200

    applied db migrations

---
+++ GamePalBackend/GamePal/Migrations/20250605142656_addedAuthProvidersTable.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GamePal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedAuthProvidersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthProviders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthProviders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAuthProviders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AuthProviderId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true),
+                    ProviderUserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAuthProviders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAuthProviders_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserAuthProviders_AuthProviders_AuthProviderId",
+                        column: x => x.AuthProviderId,
+                        principalTable: "AuthProviders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAuthProviders_AuthProviderId",
+                table: "UserAuthProviders",
+                column: "AuthProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAuthProviders_UserId",
+                table: "UserAuthProviders",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAuthProviders");
+
+            migrationBuilder.DropTable(
+                name: "AuthProviders");
+        }
+    }
+}
