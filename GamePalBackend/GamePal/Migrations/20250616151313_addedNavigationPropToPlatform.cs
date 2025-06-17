23044297799d6cd5ef911f90ded9a1ef5b7d114f
Bálint <szelasszie@gmail.com>
2025-06-17 17:09:30 +0200

    added navigation property for Platfrom entitiy & applied migrations

---
+++ GamePalBackend/GamePal/Migrations/20250616151313_addedNavigationPropToPlatform.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamePal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedNavigationPropToPlatform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platforms_Games_GameId",
+                table: "Platforms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Platforms_GameId",
+                table: "Platforms");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Platforms");
+
+            migrationBuilder.CreateTable(
+                name: "GamePlatform",
+                columns: table => new
+                {
+                    GamesId = table.Column<int>(type: "integer", nullable: false),
+                    PlatformsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlatform", x => new { x.GamesId, x.PlatformsId });
+                    table.ForeignKey(
+                        name: "FK_GamePlatform_Games_GamesId",
+                        column: x => x.GamesId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlatform_Platforms_PlatformsId",
+                        column: x => x.PlatformsId,
+                        principalTable: "Platforms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlatform_PlatformsId",
+                table: "GamePlatform",
+                column: "PlatformsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePlatform");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "Platforms",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Platforms_GameId",
+                table: "Platforms",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platforms_Games_GameId",
+                table: "Platforms",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id");
+        }
+    }
+}
