57553574ec2648078ebe1eed08205425c37d102c
Bálint <szelasszie@gmail.com>
2025-07-16 16:17:24 +0200

    slight modification in filtering logic (should retrieve that items, where ALL params match)

--- GamePalBackend/GamePal/Repositories/GameRepo/GameRepo.cs
+++ GamePalBackend/GamePal/Repositories/GameRepo/GameRepo.cs
@@ -48,26 +48,36 @@ namespace GamePal.Repositories.GameRepo
 
             if (!string.IsNullOrWhiteSpace(name))
             {
-                name = name.ToLower();
-                query = query.Where(g => g.Name.ToLower().Contains(name));
+                var loweredName = name.ToLower();
+                query = query.Where(g => g.Name.ToLower().Contains(loweredName));
             }
 
             if (!string.IsNullOrWhiteSpace(genre))
             {
-                var genreList = genre.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                                     .Select(g => g.Trim())
-                                     .ToList();
+                var genreList = genre
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(g => g.Trim().ToLower())
+                    .ToList();
 
-                query = query.Where(g => g.Categories.Any(c => genreList.Contains(c.Name)));
+                query = query.Where(g =>
+                    genreList.All(selectedGenre =>
+                        g.Categories.Any(c => c.Name.ToLower() == selectedGenre)
+                    )
+                );
             }
 
             if (!string.IsNullOrWhiteSpace(platform))
             {
-                var platformList = platform.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                                           .Select(p => p.Trim())
-                                           .ToList();
+                var platformList = platform
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => p.Trim().ToLower())
+                    .ToList();
 
-                query = query.Where(g => g.Platforms.Any(p => platformList.Contains(p.Name)));
+                query = query.Where(g =>
+                    platformList.All(selectedPlatform =>
+                        g.Platforms.Any(p => p.Name.ToLower() == selectedPlatform)
+                    )
+                );
             }
 
             var total = await query.CountAsync();
